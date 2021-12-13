0c307cf9ae6dfc2a74c69bb1edbbfecf0b5cd170
Shy <fleetwoodshybreasia@gmail.com>
2021-12-12 22:40:34 -0500

    update

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+function getforecast() {
+    let city = document.getElementById('enter-city');
+    let search = document.getElementById('search-button');
+    let clear = document.getElementById('clear-history');
+    var weatherNow = document.getElementById('weather-now');
+    let cityName = document.getElementById('city-name');
+    let currentPic = document.getElementById('pic');
+    let currentTemp = document.getElementById('temp');
+    let currentHumidity = document.getElementById('humidity');
+    let currentWind = document.getElementById('wind-speed');
+    let currentUv = document.getElementById('uv-index');
+    var fiveday = document.getElementById('five-day');
+    
+    
